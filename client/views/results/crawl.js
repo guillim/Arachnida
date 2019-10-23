f0db54d4f51cfcf30124108957b20fe34c55e97b
unknown <rio.f@activymobility.com>
2019-10-23 23:42:39 +0700

    feat(CSV): add feature export into simple CSV, separated by comma

--- client/views/results/crawl.js
+++ client/views/results/crawl.js
@@ -9,6 +9,45 @@ Template.crawl.events({
   }
 });
 
+Template.crawlPageResultsOnecrawlsimple.events({
+  'click .downloadCrawl': function() {
+	let csvData = `URL,${this.listColumns.join()}\n`;
+
+	let alreadyin = []
+	let ResulturlsCollection = []
+	Resulturls.find({crawlerID:this.crawlerID},{sort: {createdAt: -1}}).fetch().map((resulturl)=>{
+	if (alreadyin.indexOf(resulturl.crawlID) === -1) {
+		alreadyin.push(resulturl.crawlID)
+		ResulturlsCollection.push(resulturl)
+	}
+	})
+	for (let indexCollection = 0; indexCollection < ResulturlsCollection.length; indexCollection++) {
+		const elementCollection = ResulturlsCollection[indexCollection];
+		csvData += elementCollection.URL_crawled;
+		for (let indexColumn = 0; indexColumn < this.listColumns.length; indexColumn++) {
+			const elementColumn = this.listColumns[indexColumn];
+			if(elementCollection.result[elementColumn]) {
+				csvData+=`,${elementCollection.result[elementColumn]}`;
+			}else {
+				csvData+=`,`;
+			}
+		}
+		csvData+='\n';
+	}
+	const hiddenElement = document.createElement('a');
+    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csvData);
+    hiddenElement.target = '_blank';
+    hiddenElement.download = `${this.title}.csv`;
+    hiddenElement.click();
+
+		/* Meteor.call('removeResults', this.crawlID, (err, res) => {
+				if (err) { console.log(err);
+				}else{ console.log(res);
+				}
+		}) */
+  }
+});
+
 Template.crawlPageResultsOnecrawlsimple.helpers({
 	settings: function () {
           let fieldsArray = []
